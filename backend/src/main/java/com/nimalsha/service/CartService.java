27279b71000812eecab876450179b704f2aa7447
kasuni2001-maker <136631162+kasuni2001-maker@users.noreply.github.com>
2024-06-23 07:55:55 -0800

    cart api

---
+++ backend/src/main/java/com/nimalsha/service/CartService.java
@@ -0,0 +1,23 @@
+package com.nimalsha.service;
+
+import com.nimalsha.model.Cart;
+import com.nimalsha.model.CartItem;
+import com.nimalsha.model.User;
+import com.nimalsha.request.AddCartItemRequest;
+
+public interface CartService {
+    
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception;
+
+    public CartItem updCartItem(Long cartItemId, int quantity) throws Exception;
+
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception;
+
+    public Long calculateCartTotals(Cart cart) throws Exception;
+
+    public Cart findCartById(Long id) throws Exception;
+
+    public Cart findCartByUserId(String jwt) throws Exception;
+
+    public Cart clearCart(String jwt) throws Exception;
+}
