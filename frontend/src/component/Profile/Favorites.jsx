6b5c3515ece6d02435f4578036d10dcb1257ed9c
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2024-07-01 16:24:57 +0530

    Profile navigation components

--- frontend/src/component/Profile/Favorites.jsx
+++ frontend/src/component/Profile/Favorites.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import RestaurantCard from '../Restaurant/RestaurantCard'
 
-function Favorites() {
+export default function Favorites() {
   return (
-    <div>Favorites</div>
+    <div>
+       <h1 style={{ padding: '2rem 0', fontSize: '1.25rem', fontWeight: '600', textAlign: 'center' }}>
+      My favourites
+    </h1>
+      <div className="flex flex-wrap gap-3 justify-center">
+
+      {[1,1,1,1].map((item)=><RestaurantCard/>)}
+
+
+       </div>
+
+
+    </div>
   )
 }
 
-export default Favorites
