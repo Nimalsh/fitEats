24ee507e3f2fddf192682b4798ec56d665b60cce
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2024-08-17 18:57:53 +0530

    user service update to put requests

--- backend/src/main/java/com/nimalsha/service/UserService.java
+++ backend/src/main/java/com/nimalsha/service/UserService.java
@@ -1,9 +1,15 @@
 package com.nimalsha.service;
 
 import com.nimalsha.model.User;
+import com.nimalsha.model.Request;
+import com.nimalsha.request.CreateRequestRequest;
+
 
 public interface UserService {
     public User findUserByJwtToken(String jwt) throws Exception;
 
     public User findUserByEmail(String email) throws Exception;
+    Request createNewRequest(String jwtToken, CreateRequestRequest req) throws Exception;
+    
+    
 }
