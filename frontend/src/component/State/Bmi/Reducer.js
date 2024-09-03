303e03add0888260b07dd5ff8053067bc9ddf1f7
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2024-09-03 10:35:37 +0530

    bmi calory track

--- frontend/src/component/State/Bmi/Reducer.js
+++ frontend/src/component/State/Bmi/Reducer.js
@@ -17,6 +17,19 @@ import {
     GET_NUTRITION_REQUEST,
     GET_NUTRITION_SUCCESS,
     GET_NUTRITION_FAILURE,
+    GET_BMIPLAN_REQUEST,
+    GET_BMIPLAN_SUCCESS,
+    GET_BMIPLAN_FAILURE,
+    UPDATE_WEIGHT_HEIGHT_REQUEST,
+    UPDATE_WEIGHT_HEIGHT_SUCCESS,
+    UPDATE_WEIGHT_HEIGHT_FAILURE,
+    FETCH_USER_DETAILS_REQUEST,
+  FETCH_USER_DETAILS_SUCCESS,
+  FETCH_USER_DETAILS_FAILURE,
+  GET_ACTIVE_PLANS_REQUEST,
+  GET_ACTIVE_PLANS_SUCCESS,
+  GET_ACTIVE_PLANS_FAILURE
+
   } from "./ActionType";
   
   const initialState = {
@@ -28,6 +41,8 @@ import {
       snacks: [],
     },
     nutrition: [],
+    activePlans: [],
+    userDetails: null,
     loading: false,
     error: null,
   };
@@ -40,6 +55,10 @@ import {
       case FETCH_NUTRITION_REQUEST:
       case UPDATE_NUTRITION_REQUEST:
       case GET_NUTRITION_REQUEST:
+      case GET_BMIPLAN_REQUEST: 
+      case UPDATE_WEIGHT_HEIGHT_REQUEST:
+      case FETCH_USER_DETAILS_REQUEST:
+      case GET_ACTIVE_PLANS_REQUEST:
         return {
           ...state,
           loading: true,
@@ -80,12 +99,44 @@ import {
           loading: false,
           nutrition: action.payload,
         };
+
+    case GET_BMIPLAN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        bmiPlan: action.payload,
+        error: null, // Clear any existing errors
+      };
+
+      case UPDATE_WEIGHT_HEIGHT_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          // Optionally handle weight and height update response
+        };
+        case FETCH_USER_DETAILS_SUCCESS:
+            return {
+              ...state,
+              loading: false,
+              userDetails: action.payload,
+            };
+        case GET_ACTIVE_PLANS_SUCCESS: // Handle success state for fetching active plans
+            return {
+              ...state,
+              loading: false,
+              activePlans: action.payload, // Store active plans in state
+              error: null, // Clear any existing errors
+            };
       case CREATE_BMIPLAN_FAILURE:
       case ADD_MEAL_FAILURE:
       case FETCH_MEALS_FAILURE:
       case FETCH_NUTRITION_FAILURE:
       case UPDATE_NUTRITION_FAILURE:
       case GET_NUTRITION_FAILURE:
+      case GET_BMIPLAN_FAILURE:
+      case UPDATE_WEIGHT_HEIGHT_FAILURE:
+      case FETCH_USER_DETAILS_FAILURE:
+      case GET_ACTIVE_PLANS_FAILURE:
         return {
           ...state,
           loading: false,
