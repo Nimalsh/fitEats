c5e5f54107bf1a5e615e9f651a9a426dd99769b2
Faheemafrauz <112307487+Faheemafrauz@users.noreply.github.com>
2024-11-26 00:25:26 +0530

    Delete backend-mobile/src/main/java/com/example/driver directory

--- backend-mobile/src/main/java/com/example/driver/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.driver.config;
-
-
-public class JwtConstant {
-    public static final String SELECT_KEY="jygrdexcvbnmsdfcvgbhnjmnhbsfcrtfsbnmjxkmshxcvsfgchbdjcnmkdnsbdsrtfbush";
-    public static final String JWT_HEADER="Authorization";
-}
--- backend-mobile/src/main/java/com/example/driver/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.driver.service;
-
-import com.example.driver.model.User;
-
-public interface UserService {
-    public User findUserByJwtToken(String jwt) throws Exception;
-
-    public User findUserByEmail(String email) throws Exception;
-}
